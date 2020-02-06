3732fb7c9afb94aa8089c7e69f402e187140c22c
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-06 12:26:47 -0500

    Points in Baskets

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Basket : MonoBehaviour
 {
+
+    [Header("Set Dynamically")]
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject scoreGo = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGo.GetComponent<Text>();
+        scoreGT.text = "0";
 
     }
 
@@ -29,6 +36,11 @@ public class Basket : MonoBehaviour
         if(collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+
 
         }
     }
